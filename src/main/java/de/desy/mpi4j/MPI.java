668ed09ac99989efb486601f09056dfd42ebf620
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2024-05-03 09:39:27 +0200

    added MPI_Error_string

--- src/main/java/de/desy/mpi4j/MPI.java
+++ src/main/java/de/desy/mpi4j/MPI.java
@@ -20,10 +20,12 @@ public class MPI {
   private static final Linker LINKER = Linker.nativeLinker();
   private static final SymbolLookup MPILIB = SymbolLookup.libraryLookup("libmpi.so", Arena.global());
 
+  private final static int  MPI_MAX_ERROR_STRING = 256;
 
   private static final MethodHandle mpiInit;
   private static final MethodHandle mpiCommRank;
   private static final MethodHandle mpiCommSize;
+  private static final MethodHandle mpiErrorString;
   private static final MethodHandle mpiFinalize;
   private static final MemorySegment MPI_COMM_WORLD;
 
@@ -46,11 +48,31 @@ public class MPI {
                     FunctionDescriptor.of(JAVA_INT, ADDRESS, ADDRESS)
             );
 
+       mpiErrorString = LINKER.downcallHandle(
+            MPILIB.find("MPI_Error_string").orElseThrow(() -> new NoSuchElementException("MPI_Error_string")),
+                    FunctionDescriptor.of(JAVA_INT, JAVA_INT, ADDRESS, ADDRESS)
+            );
+
         mpiFinalize = LINKER.downcallHandle(
             MPILIB.find("MPI_Finalize").orElseThrow(() -> new NoSuchElementException("MPI_Finalize")),
                     FunctionDescriptor.of(JAVA_INT)
             );
      
+  }
+
+  public static void checkMpiError(int status) throws MPIException, Throwable {
+    if (status != 0) {
+
+        try (var arena = Arena.ofConfined()) {
+            MemorySegment len = arena.allocate(4);
+            MemorySegment msg = arena.allocate(MPI_MAX_ERROR_STRING);
+            int rc = (int)mpiErrorString.invokeExact(status, msg, len);
+            if (rc == 0) {
+                throw new MPIException(msg.getString(0));
+            }
+            throw new MPIException("MPI Error " + status);
+        }
+    }
   }
 
   public static void main(String[] args) throws Throwable {
@@ -59,20 +81,21 @@ public class MPI {
 
     try (var arena = Arena.ofConfined()) {
       System.out.println(mpiInit);
-      int rc = (int)mpiInit.invoke(MemorySegment.NULL, MemorySegment.NULL);
+      int rc = (int)mpiInit.invokeExact(MemorySegment.NULL, MemorySegment.NULL);
       System.out.println(rc);
 
       MemorySegment size = arena.allocate(4);
-      rc = (int)mpiCommSize.invoke(MPI_COMM_WORLD, size);
+      rc = (int)mpiCommSize.invokeExact(MPI_COMM_WORLD, size);
       System.out.println("size rc: " + rc);
       System.out.println("size: " + size.asByteBuffer().order(ByteOrder.LITTLE_ENDIAN).getInt());
 
 
       MemorySegment rank = arena.allocate(4);
-      rc = (int)mpiCommRank.invoke(MPI_COMM_WORLD, rank);
+      rc = (int)mpiCommRank.invokeExact(MPI_COMM_WORLD, rank);
       System.out.println("rank rc: " + rc);
       System.out.println("rank: " + rank.asByteBuffer().order(ByteOrder.LITTLE_ENDIAN).getInt());
 
+      checkMpiError(11);
 
       rc =(int) mpiFinalize.invokeExact();
       System.out.println("finalize rc: " + rc);
